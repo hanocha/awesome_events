fc7f7f17a5f71989c9dedf15175cf12544f397e3
Hanocha <saiki@feedforce.jp>
2016-05-25 13:47:05 +0900

    welcome/indexのルーティングが残っていたので削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
   root to: 'welcome#index'
+
   # get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
