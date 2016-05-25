d1d817f0a67570a37ad57280a52e6ffd17088fae
Hanocha <saiki@feedforce.jp>
2016-05-25 10:33:35 +0900

    welcome_controllerの作成とルート修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  root to: 'welcome#index'
+  # get 'welcome/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
