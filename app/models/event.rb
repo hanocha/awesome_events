aeebcb2f2338d1ed55d72be6891fd7384587bc88
Hanocha <saiki@feedforce.jp>
2016-05-27 14:15:45 +0900

    モデルに必須項目のバリデーションを追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ActiveRecord::Base
   validates :name, length: { maximum: 50 }, presence: true
+  validates :place, presence: true
+  validates :content, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
 end
