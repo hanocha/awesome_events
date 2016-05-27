8dc9906e2e8c284e220e436bf0e5a74bd0ea1e2c
Hanocha <saiki@feedforce.jp>
2016-05-27 12:50:57 +0900

    文字数制限に対するテストを行うメソッドが変わっていたので修正

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe Event, type: :model do
   describe '#name' do
     it { should validate_presence_of(:name) }
-    it { should ensure_length_of(:name).is_at_most(50) }
+    it { should validate_length_of(:name).is_at_most(50) }
   end
 end
