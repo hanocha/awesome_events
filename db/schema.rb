8aadea7f8653fa0874101e0eed1147ddb951258d
Hanocha <saiki@feedforce.jp>
2016-05-27 14:20:47 +0900

    $ ./bin/rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160527022241) do
+ActiveRecord::Schema.define(version: 20160527051803) do
 
   create_table "events", force: :cascade do |t|
     t.integer  "owner_id"
     t.string   "name",       null: false
     t.string   "place",      null: false
     t.datetime "start_time", null: false
-    t.datetime "end_tiime",  null: false
+    t.datetime "end_time",   null: false
     t.text     "content",    null: false
     t.datetime "created_at"
     t.datetime "updated_at"
