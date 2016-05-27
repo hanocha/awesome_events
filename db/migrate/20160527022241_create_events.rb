a2e3392c36a1298d889bc9933a73edd1210ef3e1
Hanocha <saiki@feedforce.jp>
2016-05-27 11:33:40 +0900

    $ ./bin/rake db:migrate:reset

--- db/migrate/20160527022241_create_events.rb
+++ db/migrate/20160527022241_create_events.rb
@@ -2,13 +2,14 @@ class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
       t.integer :owner_id
-      t.string :name
-      t.string :place
-      t.datetime :start_time
-      t.datetime :end_tiime
-      t.text :content
-
-      t.timestamps null: false
+      t.string :name,         null: false
+      t.string :place,        null: false
+      t.datetime :start_time, null: false
+      t.datetime :end_tiime,  null: false
+      t.text :content,        null: false
+      t.timestamps
     end
+
+    add_index :events, :owner_id
   end
 end
