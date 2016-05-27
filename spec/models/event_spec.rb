9b76d843785bd11e639e4c69bea790567836f396
Hanocha <saiki@feedforce.jp>
2016-05-27 14:03:09 +0900

    名前以外の必須項目バリデーションテストを追加

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -5,4 +5,20 @@ RSpec.describe Event, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_length_of(:name).is_at_most(50) }
   end
+
+  describe '#place' do
+    it { should validate_presence_of(:place) }
+  end
+
+  describe '#start_time' do
+    it { should validate_presence_of(:start_time) }
+  end
+
+  describe '#end_time' do
+    it { should validate_presence_of(:end_time) }
+  end
+
+  describe '#content' do
+    it { should validate_presence_of(:content) }
+  end
 end
