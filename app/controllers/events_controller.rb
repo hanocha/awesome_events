361afca8766450d9eefac7080c469409d6cf5fa0
Hanocha <saiki@feedforce.jp>
2016-06-01 12:19:10 +0900

    イベント新規作成用アクションのcreateを実装

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,5 +5,19 @@ class EventsController < ApplicationController
   end
 
   def create
+    @event = current_user.created_events.build(event_params)
+    if @event.save
+      redirect_to @event, notice: '作成しました'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(
+      :name, :place, :content, :start_time, :end_time
+    )
   end
 end
