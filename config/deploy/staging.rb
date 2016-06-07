b8ccf4c08c291c4dcfdf4fc4524737c96c884122
Hanocha <saiki@feedforce.jp>
2016-06-07 17:27:54 +0900

    capistranoのStaging用設定を記述

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -17,9 +17,9 @@
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
-# role :app, %w{deploy@example.com}, my_property: :my_value
-# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-# role :db,  %w{deploy@example.com}
+role :app, %w{ops@vagrant.local}
+role :web, %w{ops@vagrant.local}
+role :db, %w{ops@vagrant.local}
 
 
 
