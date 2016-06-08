17fbfe21a100ae0176d1dc165351e710e4a7809c
Hanocha <saiki@feedforce.jp>
2016-06-08 14:40:16 +0900

    プロセスをキルする条件が抜けていたので修正

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -18,7 +18,7 @@ before_fork do |server, worker|
   defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
 
   old_pid = "#{ server.config[:pid] }.oldbin"
-  unless old_pid
+  unless old_pid == server.pid
     begin
       Process.kill :QUIT, File.read(old_pid).to_i
     rescue Errno::ENOENT, Errno::ESRCH
