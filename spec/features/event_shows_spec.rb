d8185b2b59dfbd349edf02bd97fdc37b9ef44ad6
Hanocha <saiki@feedforce.jp>
2016-06-09 15:18:02 +0900

    テスト項目を追加

--- spec/features/event_shows_spec.rb
+++ spec/features/event_shows_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.feature "EventShows", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe 'ユーザがイベント詳細を閲覧する' do
+    describe '正常系' do
+      context 'あるイベントの詳細にアクセスしたとき' do
+        it '正しいイベント名が表示されていること' do
+        end
+
+        it '正しい主催者が表示されていること' do
+        end
+
+        it '正しい開催場所が表示されていること' do
+        end
+
+        it '正しいイベント内容が表示されていること' do
+        end
+
+        it '正しい開始/終了時刻が表示されていること' do
+        end
+
+        context '戻るボタンをクリックしたとき' do
+          it 'イベント一覧ページに遷移すること' do
+          end
+        end
+      end
+    end
+  end
 end
