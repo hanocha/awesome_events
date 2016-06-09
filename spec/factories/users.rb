3e14300c0e4cef043e724da0f3aac935edd8d0e9
Hanocha <saiki@feedforce.jp>
2016-06-09 16:29:32 +0900

    テスト用に個人Twitterアカウント情報のUserをcreateできるようにした

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,4 +5,11 @@ FactoryGirl.define do
     sequence(:nickname) { |i| "nickname#{i}" }
     sequence(:image_url) { |i| "http://example.com/image#{i}.jpg" }
   end
+
+  trait :broken_peter do
+    provider 'twitter'
+    uid '2351441480'
+    nickname 'broken_peter'
+    image_url 'http://pbs.twimg.com/profile_images/714115257082515456/QakmXenG_normal.jpg'
+  end
 end
